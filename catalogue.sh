e5a042e6c6f2762d36253713e73cfca1f6a6b371
n-raghavendra <raghugoud1994@gmail.com>
2023-12-29 02:14:01 +0000

    second commmit

--- catalogue.sh
+++ catalogue.sh
@@ -8,8 +8,16 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR: Please run the script with Root User $N"
+    exit 1
+else
+    echo -e "$G You are Root User $N"
+fi
 
-VALIDATE(){
+VALIDATE()
+{
 if [ $1 -ne 0 ]
 then
     echo -e "$2.. $R is FAILED $N"
@@ -18,14 +26,6 @@ else
     echo -e "$2.. $G is SUCCESS $N"
 fi
 }
-
-if [ $ID -ne 0 ]
-then
-    echo -e "$R ERROR: Please run the script with Root User $N"
-    exit 1
-else
-    echo -e "$G You are Root User $N"
-fi
 
 dnf module disable nodejs -y &>> $LOGFILE
 VALIDATE $? "Disable nodejs"
