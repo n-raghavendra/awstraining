08bb1c6483386e69c9a9fb5acbbf8a4af78bd87d
n-raghavendra <raghugoud1994@gmail.com>
2023-12-29 02:12:35 +0000

    second commmit

--- catalogue.sh
+++ catalogue.sh
@@ -5,8 +5,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-TIMESTAMP = $(date +%F-%H-%M-%S)
-LOGFILE = "/tmp/$0-$TIMESTAMP.log"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 
 VALIDATE(){
