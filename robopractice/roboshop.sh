51459e0a2eb54adbe8116541d52869d1636954b4
n-raghavendra <raghugoud1994@gmail.com>
2024-01-17 21:25:13 +0000

    practice

--- robopractice/roboshop.sh
+++ robopractice/roboshop.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-AMI_ID="ami-081609eef2e3cc958"
+AMI_ID="ami-0f3c7d07486cad139"
 SG_ID="sg-06fddf57d42e4ea90"
 
 INSTANCES=("mongodb" "mysql" "payment" "rabbitmq" "shipping" "user" "catalogue" "cart" "redis" "web")
@@ -14,7 +14,7 @@ INST_TYPE="t2.micro"
 else
 INST_TYPE="t3.small"
 fi
-aws ec2 run-instances --image-id $AMI_ID --instance-type $INST_TYPE --security-group-ids $SG_ID --tags Key=Name,Value=$i
+aws ec2 run-instances --image-id $AMI_ID --instance-type $INST_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 echo "$i: $PrivateIpAddress"
 
 done
