2d4f7bfc3e39d4b86fbec30a2be01aedb3dfda45
n-raghavendra <raghugoud1994@gmail.com>
2024-01-17 20:39:08 +0000

    practice

--- robopractice/roboshop.sh
+++ robopractice/roboshop.sh
@@ -5,15 +5,15 @@ SG_ID="sg-06fddf57d42e4ea90"
 
 INSTANCES=("mongodb" "mysql" "payment" "rabbitmq" "shipping" "user" "catalogue" "cart" "redis" "web")
 
-for i in ${INSTANCES[@]}
+for i in "${INSTANCES[@]}""
 do
 
-if [$i == mongodb] ||  [$i == mqsql] || [$i == rabbitmq]
+if [ $i == mongodb ] ||  [ $i == mqsql ] || [ $i == rabbitmq ]
 then
 INST_TYPE="t2.micro"
 else
 INST_TYPE="t3.small"
-
+fi
 aws ec2 run-instances --image-id $AMI_ID --instance-type $INST_TYPE --security-group-ids $SG_ID --tags Key=Name,Value=$i
 echo "$i: $PrivateIpAddress"
 
