33a91b1999bf8aec607470a1247985bb2983539d
n-raghavendra <raghugoud1994@gmail.com>
2024-02-03 00:11:56 +0000

    practice1

--- robopractice/roboshop.sh
+++ robopractice/roboshop.sh
@@ -14,9 +14,7 @@ INST_TYPE="t2.micro"
 else
 INST_TYPE="t3.small"
 fi
-IPAddress=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INST_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query '$Instances[0].PrivateIpAddress' --output text)
-
-### aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type t2.micro --security-group-ids sg-06fddf57d42e4ea90 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$i}]' --query '$Instances[0].PrivateIpAddress' --output text
+IPAddress=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INST_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
 echo "$i: $IPAddress"
 
